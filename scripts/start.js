2d62af8a7114921e206ce6623c4c2a952587f6af
chadacious <chad@medlor.com>
2020-03-24 06:26:58 -0700

    meh

--- scripts/start.js
+++ scripts/start.js
@@ -154,7 +154,7 @@ checkBrowsers(paths.appPath, isInteractive)
         console.log();
       }
 
-      console.log(chalk.cyan('Starting the development server...\n'));
+      console.log(chalk.cyan('sssStarting the development server...\n'));
       openBrowser(urls.localUrlForBrowser);
     });
 
